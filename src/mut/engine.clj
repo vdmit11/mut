fdbac4fa677393023a68669680f0ecb8659813bf
Dmitry Vasilyanov <vdmit11@gmail.com>
2017-05-06 21:54:50 +0300

    move ./instruments/engine.clj -> ./engine.clj

--- src/mut/engine.clj
+++ src/mut/engine.clj
@@ -1,4 +1,4 @@
-(ns mut.instruments.engine
+(ns mut.engine
   (:require pink.event pink.simple))
 
 ;; XXX: for now, these functions are just aliases for pink.simple API.
@@ -54,3 +54,27 @@
   [instrfn & args]
   (let [afunc (apply instrfn args)]
     (add-afunc afunc)))
+
+
+;; demo
+(comment
+  (do
+    (require '[pink.instruments.piano :refer [piano]])
+
+    (defn beat [n]
+      (+ (now) n))
+
+    (def e1 (at (beat 0.2) (piano :keynum 60)))
+    (def e2 (at (beat 0.6) (piano :keynum 64)))
+    (def e3 (at (beat 1.0) (piano :keynum 67)))
+
+    (def e4 (at (beat 1.4) (piano :keynum 60)))
+    (def e5 (at (beat 1.4) (piano :keynum 64)))
+    (def e6 (at (beat 1.4) (piano :keynum 67)))
+
+    (start)
+    (add-events [e1 e2 e3 e4 e5 e6])
+    (Thread/sleep 4000)
+    (clear)
+    (stop))
+  )
