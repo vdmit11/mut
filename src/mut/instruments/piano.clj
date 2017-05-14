257f6e0c19bd7e2953f5abe8b8fa29ba1b47e1f1
Dmitry Vasilyanov <vdmit11@gmail.com>
2017-05-14 20:25:26 +0300

    implement Piano instrument prototype

---
+++ src/mut/instruments/piano.clj
@@ -0,0 +1,45 @@
+(ns mut.instruments.piano
+  (:require pink.instruments.piano
+            [mut.instruments :refer [at MusicObject->AudioEngineEvents]]
+            [mut.music.pitch :as pitch]))
+
+(defrecord Piano [])
+
+(defn note->event
+  [_ time note]
+  (at time
+      (pink.instruments.piano/piano
+       :keynum (pitch/keynum-of note))))
+
+(defn mo->events
+  [_ time mo]
+  (if-not (seqable? mo)
+    (note->event _ time mo)
+    (map-indexed #(mo->events _ (+ time %1) %2) (seq mo))))
+
+(extend Piano MusicObject->AudioEngineEvents {:mo->events mo->events})
+
+(def piano (->Piano))
+
+
+;; demo
+(comment
+  (require '[mut.instruments :refer [start-engine stop-engine clear-engine play]])
+
+  (start-engine)
+  (Thread/sleep 100)
+
+  (do
+    (play piano 60)
+    (Thread/sleep 1000)
+    (play piano 62)
+    (Thread/sleep 1000)
+    (play piano 65)
+    (Thread/sleep 1000))
+
+  (play piano [60 62 65])
+  (Thread/sleep 3000)
+
+  (Thread/sleep 1000)
+  (clear-engine)
+  (stop-engine))
