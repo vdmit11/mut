7371cf148e1be27dffdf433497d341879a3d12a0
Dmitry Vasilyanov <vdmit11@gmail.com>
2018-04-03 21:39:32 +0300

    bump versions of project deps

--- project.clj
+++ project.clj
@@ -18,9 +18,10 @@
   :main mut.core
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/data.generators "0.1.2"]
-                 [kunstmusik/pink "0.4.0"]
+                 [kunstmusik/pink "0.4.1"]
                  [acyclic/squiggly-clojure "0.1.8"]
-                 [swiss-arrows "1.0.0"]]
+                 [swiss-arrows "1.0.0"]
+                 [com.rpl/specter "1.1.0"]]
   :plugins [[lein-ancient "0.6.15"]
             [cider/cider-nrepl "0.16.0"]
             [jonase/eastwood "0.2.5"]
