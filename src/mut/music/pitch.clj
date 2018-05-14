7bf64df982e86d5f19eb99a94374aaf456a65131
Dmitry Vasilyanov <vdmit11@gmail.com>
2018-05-15 01:16:41 +0300

    change dynamic->static variables in pitch.clj
    Because I plan to do pitch/hz conversion delayed in time,
    maybe even in separate threads. So this dynamic rebinding
    of variables becomes not useful, and even undesireable.
    
    So static (and not changing in time) variables are better.

--- src/mut/music/pitch.clj
+++ src/mut/music/pitch.clj
@@ -1,19 +1,19 @@
 (ns mut.music.pitch
   (:require [mut.music.tuning :as tuning]))
 
-(def ^:dynamic *tuning* tuning/EDO-12)
-(def ^:dynamic *reference-hz* 440)
-(def ^:dynamic *reference-name* "A4")
-(def ^:dynamic *reference-keynum* 69)
+(def DEFAULT-TUNING tuning/EDO-12)
+(def REFERENCE-HZ 440)
+(def REFERENCE-NAME "A4")
+(def REFERENCE-KEYNUM 69)
 
 (defn distance->hz [distance]
-  (* *reference-hz* (tuning/distance->ratio *tuning* distance)))
+  (* REFERENCE-HZ (tuning/distance->ratio DEFAULT-TUNING distance)))
 
 (defn hz->distance [hz]
-  (tuning/ratio->distance *tuning* (/ hz *reference-hz*)))
+  (tuning/ratio->distance DEFAULT-TUNING (/ hz REFERENCE-HZ)))
 
 (defn keynum->hz [keynum]
-  (distance->hz (- keynum *reference-keynum*)))
+  (distance->hz (- keynum REFERENCE-KEYNUM)))
 
 (defn hz->keynum [hz]
-  (+ *reference-keynum* (hz->distance hz)))
+  (+ REFERENCE-KEYNUM (hz->distance hz)))
