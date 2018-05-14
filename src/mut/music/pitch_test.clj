b0b1927ec31a3a70f08091f21828d8245bfcf919
Dmitry Vasilyanov <vdmit11@gmail.com>
2018-05-15 00:19:58 +0300

    drop mut.builtins module in favor of mut.pitch
    Because I don't plan to use built-in Java/Clojure types (like numbers)
    as music objects anymore. I decided that all objects should be maps.
    
    So `mut.music.builtins` (where I planned to implement Music Object
    protocol for built-in types) doesn't make sense anymore.

--- src/mut/music/pitch_test.clj
+++ src/mut/music/pitch_test.clj
@@ -1,13 +1,12 @@
-(ns mut.music.builtins-test
+(ns mut.music.pitch-test
   (:require [clojure.test :refer [are deftest]]
             [mut.utils.test :refer [approx=]]
-            mut.music.builtins
             [mut.music.pitch :as pitch]))
 
-(deftest number-as-pitch
+(deftest midi-keynum-to-hz-conversion
   (are [n hz]
-      (and (= (pitch/keynum-of n) n)
-           (approx= (pitch/hz-of n) hz))
+      (and (approx= (pitch/hz->keynum hz) n)
+           (approx= (pitch/keynum->hz n) hz))
     0   8.1757989156
     1   8.6619572180
     2   9.1770239974
