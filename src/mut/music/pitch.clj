839e821b9ad3f75a6717ee9505ffea899d1e290e
Dmitry Vasilyanov <vdmit11@gmail.com>
2018-05-15 00:48:44 +0300

    drop useless HasPitch protocol
    Planned to use it in the past, but then changed the design.

--- src/mut/music/pitch.clj
+++ src/mut/music/pitch.clj
@@ -1,12 +1,6 @@
 (ns mut.music.pitch
   (:require [mut.music.tuning :as tuning]))
 
-(defprotocol HasPitch
-  (hz-of [mo])
-  (name-of [mo])
-  (keynum-of [mo]))
-
-
 (def ^:dynamic *tuning* tuning/EDO-12)
 (def ^:dynamic *reference-hz* 440)
 (def ^:dynamic *reference-name* "A4")
