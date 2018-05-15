aa936fcf70d2fa2306ae76265f7c68e0feaa0782
Dmitry Vasilyanov <vdmit11@gmail.com>
2018-05-16 01:26:30 +0300

    don't use special names for constants
    https://github.com/bbatsov/clojure-style-guide#dont-flag-constants

--- src/mut/music/pitch.clj
+++ src/mut/music/pitch.clj
@@ -1,19 +1,19 @@
 (ns mut.music.pitch
   (:require [mut.music.tuning :as tuning]))
 
-(def DEFAULT-TUNING tuning/EDO-12)
-(def REFERENCE-HZ 440)
-(def REFERENCE-NAME "A4")
-(def REFERENCE-KEYNUM 69)
+(def default-tuning tuning/EDO-12)
+(def reference-hz 440)
+(def reference-name "A4")
+(def reference-keynum 69)
 
 (defn distance->hz [distance]
-  (* REFERENCE-HZ (tuning/distance->ratio DEFAULT-TUNING distance)))
+  (* reference-hz (tuning/distance->ratio default-tuning distance)))
 
 (defn hz->distance [hz]
-  (tuning/ratio->distance DEFAULT-TUNING (/ hz REFERENCE-HZ)))
+  (tuning/ratio->distance default-tuning (/ hz reference-hz)))
 
 (defn keynum->hz [keynum]
-  (distance->hz (- keynum REFERENCE-KEYNUM)))
+  (distance->hz (- keynum reference-keynum)))
 
 (defn hz->keynum [hz]
-  (+ REFERENCE-KEYNUM (hz->distance hz)))
+  (+ reference-keynum (hz->distance hz)))
