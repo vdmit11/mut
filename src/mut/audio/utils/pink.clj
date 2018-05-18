c2220bf589889b2194b88e57762e1f757f64a59d
Dmitry Vasilyanov <vdmit11@gmail.com>
2018-05-18 04:36:11 +0300

    remove obsolete code from mut.audio namespace
    This old mut.audio code was only a prototype,
    and I'm going to re-write it.
    
    So here I'm deleting the code I think is useless, and moving
    all useful code to the new module: mut.audio.utils.pink

---
+++ src/mut/audio/utils/pink.clj
@@ -0,0 +1,42 @@
+(ns mut.audio.utils.pink
+  "Various utils for the `pink` audio engine."
+  (:require pink.event))
+
+(defn switch-engine-to-absolute-time!
+  "Switch pink.engine.Engine events to absolute time.
+
+  By default, the Engine uses relative time for events.
+  That is, when you add an event, you say something like 'it starts in 2 beats from now'.
+
+  This is handy for hand-coding scores, but not very handy for generating music.
+  Because we usually generate music in advance, and we know when each note sounds.
+  So for us, it is more handy to say 'these notes start at beats 32, 33, 34, 35'.
+
+  So this function is a tool to switch pink's Engine to the more handy absolute time."
+  [engine]
+  (pink.event/use-absolute-time! (.event-list engine)))
+
+
+(comment
+  "FIXME: this macro can only be used with `pink.simple` (one global engine),
+   while I want multiple engines in parallel. Need to somehow rework it."
+  (defmacro at
+    "Produce a pink Event that fires an instrument function.
+
+     Example:
+       (at 16 (piano :keynum 60 :duration 2))
+
+     - `piano` is the instrument function (that returns an audio function)
+     - `:keynum/:duration` are arguments for the instrument function
+     - `16` is the event start time (measured in beats since engine start)
+
+     So the macro produces the `pink.event/Event` object that you can add to the engine.
+     This Event, when fired, will evaluate the specified instrument function,
+     and add the resulting audio function to the current engine."
+    [time [instrfn & args]]
+    `(pink.event/event fire-instrfn ~time ~instrfn ~@args))
+
+  (defn fire-instrfn
+    [instrfn & args]
+    (let [afunc (apply instrfn args)]
+      (add-afunc afunc))))
