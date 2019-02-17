75f75a887903b221987ae2a53c82335a4d956607
Dmitry Vasilyanov <vdmit11@gmail.com>
2019-02-17 04:23:44 +0300

    bump project deps

--- project.clj
+++ project.clj
@@ -24,7 +24,7 @@
   :src-paths ["src/"]
   :test-paths ["src/"]
   :main mut.core
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/data.generators "0.1.2"]
                  [kunstmusik/pink "0.4.1"]
                  [acyclic/squiggly-clojure "0.1.8"]
@@ -33,7 +33,7 @@
                  [org.clojure/test.check "0.9.0"]
                  [org.clojure/math.numeric-tower "0.0.4"]]
   :plugins [[lein-ancient "0.6.15"]
-            [cider/cider-nrepl "0.18.0-SNAPSHOT"]
+            [cider/cider-nrepl "0.20.0-SNAPSHOT"]
             [refactor-nrepl "2.4.0-SNAPSHOT"]
             [jonase/eastwood "0.2.5"]
             [lein-kibit "0.1.6"]
