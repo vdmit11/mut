d3bc9d0dd23b25f2bafd27da04a8366d321443c5
Dmitry Vasilyanov <vdmit11@gmail.com>
2019-10-17 23:43:14 +0300

    evict Click from playground.clj to instr/click.clj

---
+++ src/mut/audio/instr/click.clj
@@ -0,0 +1,35 @@
+(ns mut.audio.instr.click
+  (:require [pink.instruments.drums :as drums])
+  (:require [pink.oscillators :as oscillators])
+  (:require [pink.filters :as filters])
+  (:require [pink.util :refer [mul sum]])
+  (:require [mut.utils.map :as utils.map])
+  (:require [mut.audio.pink-utils :refer [end-when-silent]])
+  (:require [mut.audio.instr-proto :refer [Instrument]]))
+
+(def ^:const zdf-mode-lowpass 0)
+(def ^:const zdf-mode-bandpass 2)
+
+(defn synth-click [hz]
+  (end-when-silent
+    (sum
+      (->
+        (oscillators/pulse 0 50)
+        (filters/zdf-2pole hz 40.0 zdf-mode-bandpass)
+        (filters/zdf-2pole 2000 0.6 zdf-mode-lowpass))
+      (->
+        (drums/g-noise 40)
+        (mul (drums/exp-decay 0.001 1000))
+        (filters/zdf-2pole hz 4 zdf-mode-bandpass)))))
+
+(def click-hzs
+  {-1.0 1100
+   0.0 1600
+   1.0 2000})
+
+(defn get-click-hz [beat]
+  (utils.map/get-closest click-hzs (or (:stress beat) 0)))
+
+(defrecord Click [id type engine node]
+  Instrument
+  (mo->afn [_ mo] (synth-click (get-click-hz mo))))
--- src/mut/playground.clj
+++ src/mut/playground.clj
@@ -4,42 +4,9 @@
 
 (require 'pink.engine)
 (require 'pink.node)
-(require '[mut.utils.map :as utils.map])
-(require '[mut.utils.math :as utils.math])
-(require '[pink.instruments.drums :as drums])
-(require '[pink.oscillators :as oscillators])
-(require '[pink.filters :as filters])
-(require '[pink.util :refer [mul sum]])
-(require '[mut.audio.pink-utils :refer [end-when-silent]])
 (require '[mut.audio :as audio])
 (require '[mut.audio.instr-proto :as instr-proto])
-
-(def ^:const zdf-mode-lowpass 0)
-(def ^:const zdf-mode-bandpass 2)
-
-(defn synth-click [hz]
-  (end-when-silent
-    (sum
-      (->
-        (oscillators/pulse 0 50)
-        (filters/zdf-2pole hz 40.0 zdf-mode-bandpass)
-        (filters/zdf-2pole 2000 0.6 zdf-mode-lowpass))
-      (->
-        (drums/g-noise 40)
-        (mul (drums/exp-decay 0.001 1000))
-        (filters/zdf-2pole hz 4 zdf-mode-bandpass)))))
-
-(def click-hzs
-  {-1.0 1100
-   0.0 1600
-   1.0 2000})
-
-(defn get-click-hz [beat]
-  (utils.map/get-closest click-hzs (or (:stress beat) 0)))
-
-(defrecord Click [id type engine node]
-  instr-proto/Instrument
-  (mo->afn [_ mo] (synth-click (get-click-hz mo))))
+(use 'mut.audio.instr.click)
 
 (def instr-factories
   {:click map->Click})
@@ -48,7 +15,7 @@
 (def click-instr (audio/alloc-instr! orchestra :click-1 4))
 (audio/start! orchestra)
 
-(for [n (range 4)]
+(for [n (range 1)]
   (do
     (instr-proto/schedule-play-instrument! click-instr (+ n 1/4) {:stress 1})
     (instr-proto/schedule-play-instrument! click-instr (+ n 2/4) {:stress 0})
